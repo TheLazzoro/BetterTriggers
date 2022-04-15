c3ed8992e4ee6bf8f11fc3b4b93b50ce90a6bd56
TheLazzoro <lasse.d@live.dk>
2022-04-15 21:33:20 +0200

    Error msg prompt on unhandled exceptions

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -13,5 +13,11 @@ namespace GUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            e.Handled = true;
+        }
     }
+
 }
