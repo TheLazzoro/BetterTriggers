cd54615fe9e06c122e6d10b94071a74e04366b0f
TheLazzoro <43842641+TheLazzoro@users.noreply.github.com>
2023-10-28 04:30:02 +0200

    Kept some of the changes from the GUID branch

--- BetterTriggers/Models/SaveableData/War3Project.cs
+++ BetterTriggers/Models/SaveableData/War3Project.cs
@@ -19,6 +19,6 @@ namespace BetterTriggers.Models.SaveableData
         /// Specifies the current editor version. Increment when new features to the file format are added.
         /// </summary>
         [JsonIgnore]
-        public static readonly int EditorVersion = 2;
+        public const int EditorVersion = 3;
     }
 }
